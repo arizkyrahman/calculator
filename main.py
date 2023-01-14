bcc1f0e72919b487821035435e260434d9db5e4b
arizkyrahman <rizkyrahman7202@gmail.com>
2023-01-14 16:05:16 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from tkinter import *
+import math
+
+answerVariableGlobal = ""
+answerLabelForSquareRoot = ""
+
+root = Tk()
+root.title('Calculator App')
+
+answerEntryLabel = StringVar()
+Label(root, font=('futura', 25, 'bold'), textvariable=answerEntryLabel, justify=LEFT, height=2, width=7).grid(columnspan=4, ipadx=120)
+
+answerFinalLabel = StringVar()
+Label(root, font=('futura', 25, 'bold'), textvariable=answerFinalLabel, justify=LEFT, height=2, width=7).grid(columnspan=4, ipadx=120)
+
+def changeAnswerEntryLabel(entry):
+    global answerVariableGlobal
+    global answerLabelForSquareRoot
+
+    answerVariableGlobal = answerVariableGlobal + str(entry)
+    answerLabelForSquareRoot = answerLabelForSquareRoot
+    answerEntryLabel.set(answerVariableGlobal)
+
+def clearAnswerEntryLabel():
+    global answerVariableGlobal
+    global answerLabelForSquareRoot
+
+    answerLabelForSquareRoot = answerLabelForSquareRoot
+    answerVariableGlobal = ""
+    answerEntryLabel.set(answerVariableGlobal)
