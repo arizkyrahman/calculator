17624c7452cdd98f49a1c0c6a3fc893ced19cc36
arizkyrahman <rizkyrahman7202@gmail.com>
2023-01-18 19:02:20 +0700

    add complete button

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def evaluateSquareRoot():
 
     except (ValueError, SyntaxError, TypeError, ZeroDivisionError):
         try:
-            sqrtAnswer = math.sqrt(eval(str(answerLabelForSquareRoot)))
+            sqrtAnswer = math.sqrt(eval(str(answerVariableGlobal)))
             clearAnswerEntryLabel()
             answerFinalLabel.set(sqrtAnswer)
 
@@ -72,7 +72,7 @@ def allClear():
 
 def createButton(txt, x, y):
     Button(root, font=('futura', 15, 'bold'),
-        padx=16, pady= 16, text=str(txt),
+        padx=16, pady=16, text=str(txt),
         command=lambda: changeAnswerEntryLabel(txt),
         height=2, width=9).grid(row=x, column=y, sticky=E)
 
@@ -85,3 +85,25 @@ for i in range(3, 8):
     for j in range(0, 4):
         createButton(buttons[buttonsListTraversalCounter], i, j)
         buttonsListTraversalCounter = buttonsListTraversalCounter + 1
+
+Button(root, font=('futura', 15, 'bold'),
+        padx=16, pady=16, text="",
+        command=lambda: evaluateSquareRoot(),
+        height=2, width=9).grid(row=3, column=1, sticky=E)
+
+Button(root, font=('futura', 15, 'bold'),
+        padx=16, pady=16, text="AC",
+        command=lambda: allClear(),
+        height=2, width=9).grid(row=3, column=0, sticky=E)
+
+Button(root, font=('futura', 15, 'bold'),
+        padx=16, pady=16, text="0",
+        command=lambda: changeAnswerEntryLabel(0),
+        height=2, width=21).grid(row=7, column=0, columnspan=2, sticky=E)
+
+Button(root, font=('futura', 15, 'bold'),
+        padx=16, pady=16, text="=",
+        command=lambda: evaluateAnswer(),
+        height=2, width=9).grid(row=7, column=3, sticky=E)
+
+root.mainloop()
